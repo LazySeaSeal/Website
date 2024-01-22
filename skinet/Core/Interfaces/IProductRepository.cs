c8762f57b7107158bf2f4ed8b7d1c1e6aafee469
LazySeaSeal <achrafjlassi2001@gmail.com>
2024-01-22 21:03:59 +0100

    base work 1

---
+++ skinet/Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+
+using Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();// a specific list for reading only
+    }
+}

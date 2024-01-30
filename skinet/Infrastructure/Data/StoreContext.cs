317406f838de9bcf4f73838a4f6196a61654a325
LazySeaSeal <achrafjlassi2001@gmail.com>
2024-01-30 22:58:00 +0100

    adding sorting specification+ putting them in use in the product controller

--- skinet/Infrastructure/Data/StoreContext.cs
+++ skinet/Infrastructure/Data/StoreContext.cs
@@ -19,6 +19,25 @@ namespace Infrastructue.Data
             te3 class ali bech taamlilha */
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            // this part for fixing errors from converting data to sqlite
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                    foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                    {
+                        var properties = entityType.ClrType.GetProperties().Where(p =>p.PropertyType == 
+                        typeof(decimal));
+
+                        foreach( var property in properties)
+                         {
+                            modelBuilder.Entity(entityType.Name).Property(property.Name)
+                            .HasConversion<double>();
+                         }
+
+
+            
+                    }
+            }
+
         }
     }
 }

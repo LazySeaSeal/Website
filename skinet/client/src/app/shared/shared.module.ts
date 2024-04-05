1c808e86d8f266cad53454b1ec4baa808731a4aa
LazySeaSeal <achrafjlassi2001@gmail.com>
2024-04-05 16:16:22 +0100

    adding content for the home page

--- skinet/client/src/app/shared/shared.module.ts
+++ skinet/client/src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './paging-header/paging-header.component';
 import { PagerComponent } from './pager/pager.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 
 
 
@@ -16,11 +18,13 @@ import { PagerComponent } from './pager/pager.component';
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
-    PagerComponent
+    PagerComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
